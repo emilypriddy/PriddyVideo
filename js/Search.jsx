79e13632f73e33484d68824c5213344668211585
emilypriddy <emily.priddy09@gmail.com>
2018-07-25 12:53:22 -0500

    Modify search page and add ShowCard

--- js/Search.jsx
+++ js/Search.jsx
@@ -1,5 +1,21 @@
 import React from 'react';
+import preload from '../data.json';
 
-const Search = () => <h1>Search!!</h1>;
+const Search = () => (
+	<div className="search">
+		<div>
+			{preload.shows.map(show => (
+				<div className="show-card">
+					<img alt={`${show.title} Show Poster`} src={`/public/img/posters/${show.poster}`} />
+					<div>
+						<h3>{show.title}</h3>
+						<h4>({show.year})</h4>
+						<p>{show.description}</p>
+					</div>
+				</div>
+			))}
+		</div>
+	</div>
+);
 
 export default Search;
