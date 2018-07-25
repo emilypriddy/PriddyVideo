97bb8d41387d863336c5693fd2d05115205d3470
emilypriddy <emily.priddy09@gmail.com>
2018-07-25 13:20:19 -0500

    Use styled-components for ShowCards

--- js/Search.jsx
+++ js/Search.jsx
@@ -1,21 +1,17 @@
 import React from 'react';
+import ShowCard from './ShowCard';
 import preload from '../data.json';
 
 const Search = () => (
 	<div className="search">
 		<div>
-			{preload.shows.map(show => (
-				<div className="show-card">
-					<img alt={`${show.title} Show Poster`} src={`/public/img/posters/${show.poster}`} />
-					<div>
-						<h3>{show.title}</h3>
-						<h4>({show.year})</h4>
-						<p>{show.description}</p>
-					</div>
-				</div>
-			))}
+			{preload.shows.map((show) => {
+				return (
+					<ShowCard {...show} key={show.imdbID} />
+				)
+			})}
 		</div>
 	</div>
-);
+)
 
 export default Search;
